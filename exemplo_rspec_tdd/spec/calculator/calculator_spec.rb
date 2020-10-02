46306aa92e0bba471114c2376ab6ab1670fcd00b
Carlos Tiago <ontiago@gmail.com>
2020-10-02 17:52:31 -0300

    firsts tests

---
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -0,0 +1,21 @@
+require 'calculator'
+
+describe Calculator do
+	subject(:calc) { described_class.new() }
+	context '#sum' do		
+		it 'with positive numbers' do
+			result = calc.sum(5,7)
+			expect(result).to eq(12)
+		end
+
+		it 'with positive and negative numbers' do
+			result = calc.sum(-5,7)
+			expect(result).to eq(2)
+		end
+
+		it 'with negative numbers' do
+			result = calc.sum(-5,-7)
+			expect(result).to eq(-12)
+		end
+	end
+end
