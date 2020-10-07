b02112c21db718a0405e2491ea2fd2c3e3b712d4
Carlos Tiago <ontiago@gmail.com>
2020-10-07 17:41:35 -0300

    testes com macthers de igualdade

---
+++ exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
@@ -0,0 +1,21 @@
+describe "Matchers de Comparação" do
+    it "#equal testa se é o mesmo objeto" do
+      x = "ruby"
+      expect(x).to equal(x)  
+    end
+    it "#be testa se é o mesmo objeto" do
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to be(y)  
+    end
+    it "#eql testa se é o mesmo valor" do
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eql(y)  
+    end
+    it "#eq testa se é o mesmo valor" do
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eq(y)  
+    end
+end
